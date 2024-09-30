50459818177850b244712d38c614da7e375d8b6b
Antonio Garrido Masse <agarmas03@gmail.com>
2024-09-30 21:10:11 +0200

    Other pages python files

--- Welcome.py
+++ Welcome.py
@@ -1,7 +1,36 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-data = pd.read_csv('./remote_work_productivity.csv', index_col='Employee_ID')
+st.set_page_config(
+    page_title='Welcome',
+    page_icon=':wave:'
+)
 
-st.title('Remote work productivity dataframe')
+dtype_dict = {
+    'Employee_ID': 'int64',
+    'Employment_Type': 'category',
+    'Hours_Worked_Per_Week': 'int64',
+    'Productivity_Score': 'int64',
+    'Well_Being_Score': 'int64'
+}
+
+data = pd.read_csv('./remote_work_productivity.csv',
+                   index_col='Employee_ID', dtype=dtype_dict)
+
+st.title('Remote work productivity data')
+
+st.header('DataFrame')
+
 st.dataframe(data)
+
+st.header('Heatmap of Correlation Matrix')
+
+correlation_matrix = data.drop(columns=['Employment_Type']).corr()
+
+plt.figure(figsize=(10, 6))
+heatmap = sns.heatmap(correlation_matrix, annot=True,
+                      cmap="coolwarm", linewidths=0.5)
+
+st.pyplot(plt)
---
+++ pages/2_In-Ofice_Analysis.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='In-Ofice Analysis',
+    page_icon=':briefcase:'
+)
+
+
